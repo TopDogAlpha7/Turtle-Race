5cc4b8f3703674c02167f328d409562346fa8b34
Tinodaishe Simbanegavi <108521409+TopDogAlpha7@users.noreply.github.com>
2022-08-02 18:13:45 +0200

    added  files

---
+++ README.md
@@ -0,0 +1,3 @@
+#Turtle Race
+
+This is a simulation of a turtle race that I created using the turtle module
---
+++ turtle_race.py
@@ -0,0 +1,54 @@
+from secrets import choice
+from tkinter import Y
+from turtle import Turtle, Screen
+import random
+
+
+screen = Screen()
+screen.setup(width=500, height=400)
+bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race?(Red, Orange, Yellow, Green, Blue, Indigo or Violet) Enter a colour: ").lower()
+colours = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo"]
+y_coordinates = [0, 50, 100, -50, -100, -150]
+speed = [50, 45, 42, 37, 32, 29]
+new_turtles = []
+
+for turtle in range(0, 6):
+    t = Turtle(shape="turtle")
+    t.color(colours[turtle])
+    t.penup()
+    t.goto(x=-755, y=y_coordinates[turtle])
+    new_turtles.append(t)
+
+if bet:
+    race_on = True
+
+while race_on:
+    for turtle in new_turtles:
+        if turtle.xcor() > 715:
+            race_on = False
+            winning = turtle.pencolor()
+            if winning == bet:
+                print(f"You've won! The {winning} turtle is the winner")
+                break
+            else:
+                print(f"You've lost! The {winning} turtle is the winner")
+                break
+            
+        distance = random.randint(0,10)
+        turtle.fd(distance)
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
+
